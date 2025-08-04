da9b7b513d385f6c4cc67e46ed677165eefb6510
Sedzani Ragau <sedzaniragau@Mac.home>
2025-08-04 21:11:48 +0200

    correct usage of hugging face client

--- recipes/services/huggingface_client.py
+++ recipes/services/huggingface_client.py
@@ -2,11 +2,15 @@ from huggingface_hub import InferenceClient
 import os
 
 HF_API_KEY = os.getenv("HUGGINGFACE_API_TOKEN")
-HF_MODEL_ID = "sentence-transformers/all-MiniLM-L6-v2"
+
 
 # Singleton client
-hf_client = InferenceClient(api_key=HF_API_KEY)
 
 
-def generate_embedding(text: str, model_id: str = HF_MODEL_ID) -> list[float]:
-    return hf_client.feature_extraction(model=HF_MODEL_ID, inputs=text)
+def generate_embedding(text: str, model_id: str = None) -> list[float]:
+
+    if model_id is None:
+        model_id = "sentence-transformers/all-MiniLM-L6-v2"
+    hf_client = InferenceClient(model=model_id, api_key=HF_API_KEY)
+    response = hf_client.feature_extraction(text)
+    return response.data
